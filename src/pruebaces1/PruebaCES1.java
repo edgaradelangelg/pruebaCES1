de224d99679fd7bb4b67ed7291508dfa5f416a60
edgar.a.delangel.g@hotmail.com <>
2022-04-28 18:42:52 -0500

    PRUEBA CES 1

---
+++ src/pruebaces1/PruebaCES1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaces1;
+
+/**
+ *
+ * @author ALE DEL ANGEL
+ */
+public class PruebaCES1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("GITHUB DEL ANGEL 28-04-2022");
+    }
+    
+}
